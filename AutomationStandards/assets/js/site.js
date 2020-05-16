1fd2176354152c634808878924eb913b5ffcddfb
alex-tester <44508668+alex-tester@users.noreply.github.com>
2020-05-16 06:43:51 -0400

    Adding initial project files

---
+++ AutomationStandards/assets/js/site.js
@@ -0,0 +1,253 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function initValidation() {
+    jQuery('.js-validation').validate({
+        ignore: [],
+        errorClass: 'invalid-feedback animated fadeIn text-right',
+        errorElement: 'div',
+        errorPlacement: function (error, el) {
+            jQuery(el).addClass('is-invalid');
+            jQuery(el).parents('.form-group').append(error);
+        },
+        highlight: function (el) {
+            jQuery(el).parents('.form-group').find('.is-invalid').removeClass('is-invalid').addClass('is-invalid');
+        },
+        success: function (el) {
+            jQuery(el).parents('.form-group').find('.is-invalid').removeClass('is-invalid');
+            jQuery(el).remove();
+        }
+    });
+
+
+}
+function initValidationHorizontal() {
+    jQuery('.js-validation').validate({
+        ignore: [],
+        errorClass: 'invalid-feedback animated fadeIn text-right offset-sm-3 col-sm-7',
+        errorElement: 'div',
+        errorPlacement: function (error, el) {
+            jQuery(el).addClass('is-invalid');
+            jQuery(el).parents('.form-group').append(error);
+        },
+        highlight: function (el) {
+            jQuery(el).parents('.form-group').find('.is-invalid').removeClass('is-invalid').addClass('is-invalid');
+        },
+        success: function (el) {
+            jQuery(el).parents('.form-group').find('.is-invalid').removeClass('is-invalid');
+            jQuery(el).remove();
+        }
+    });
+
+}
+
+
+function ClosePopupEditor() {
+    $('#globalContentModal').modal('hide');
+}
+
+function ShowPopUpDelete(viewName, controller, objectID, objectName, reloadViewName, UseId, reloadObjectID) {
+    swal({
+        title: 'Are you sure?',
+        text: 'Do you really want to delete <b> ' + objectName + '</b>?',
+        type: 'question',
+        showCancelButton: true,
+        confirmButtonClass: 'btn btn-outline-danger m-1',
+        cancelButtonClass: 'btn btn-outline-secondary m-1',
+        confirmButtonText: 'Yes, delete it!',
+        html: 'Do you really want to delete <br><br><b> ' + objectName + '</b>?<br><br>',
+        preConfirm: function (e) {
+            return new Promise(function (resolve) {
+                setTimeout(function () {
+                    resolve();
+                }, 50);
+            });
+        }
+    }).then(function (result) {
+        //debugger;
+        if (UseId == true) {
+            var pData = { id: objectID, ParentId: reloadObjectID };
+        }
+        else {
+            var pData = { id: objectID };
+        }
+
+        if (result.value) {
+            var url = getActionURL(viewName, controller);
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: pData,
+                success: function (result) {
+                    if (result.failure) {
+                        ShowPopUpUserError(result.message);
+                    }
+                    else if (result === false) { //default message if we havent defined the failure reason
+
+                        ShowPopUpUserError('Something went wrong when attempting to delete. Please contact the site admin for assistance.');
+                    }
+                    else {
+                        LoadPageBody(reloadViewName, controller, UseId, reloadObjectID);
+                        ShowPopUpSuccessWithTimer('Deleted!', '<b>' + objectName + '</b> has been deleted.');
+                        //swal('Deleted!', '<b>' + objectName + '</b> has been deleted.', 'success');
+
+                        
+                    }
+                },
+                failure: function (result) {
+                }
+            });
+
+            // result.dismiss can be 'overlay', 'cancel', 'close', 'esc', 'timer'
+        } else if (result.dismiss === 'cancel') {
+            //swal('Cancelled', 'Your imaginary file is safe :)', 'error');
+        }
+    });
+}
+
+function ShowPopUpUserError(htmlMessage) {
+    swal({
+        title: 'Failed!',
+        type: 'error',
+        html: "<b>" + htmlMessage + "</b>"
+    });
+}
+
+
+function ShowPopUpEditor(viewName, controller, objectID, objectType, parentObjectID, saveMethod) {
+    //get modal object from DOM
+    modal = $('#globalContentModal');
+
+    $('#contentModalTitleText').text(objectType);
+
+    $('#saveBtnModelContent').attr("onclick", saveMethod)
+
+    var url = getActionURL(viewName, controller);
+    url = url + "/" + objectID + '?ParentID=' + parentObjectID;
+
+    $('#contentModalContent').load(url);
+
+
+    $(modal).modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+
+
+
+    $(modal).modal('show');
+}
+
+function ShowPopUpEditorDynamic(viewName, controller, objectID, objectType, ProcessMethod, ProcessController, ReloadAction, ReloadController, useId, Id) {
+    //get modal object from DOM
+    modal = $('#globalContentModal');
+
+    $('#contentModalTitleText').text(objectType);
+
+    //debugger;
+    $('#saveBtnModelContent').attr("onclick", "ProcessForm('" + ProcessMethod + "','" + ProcessController + "','" + ReloadAction + "','" + ReloadController + "','" + useId + "','" + Id + "')");
+    var url = getActionURL(viewName, controller);
+    url = url + "/" + objectID
+    if (useId) {
+        url = url + "?ParentId=" + Id;
+    }
+
+    $('#contentModalContent').load(url);
+
+   
+    $(modal).modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+
+
+    
+    $(modal).modal('show');
+}
+
+function ProcessForm(Action, Controller, ReloadAction, ReloadController, UseId, Id) {
+    
+    if ($("#frmEditor").valid()) {
+
+        var url = getActionURL(Action, Controller);
+        var fData = $("#frmEditor").serialize();
+
+        $("#frmEditor").submit(function (e) {
+            e.preventDefault();
+        });
+
+
+        $.ajax({
+
+            type: 'POST',
+            url: url,
+            data: fData,
+            success: function (result) {
+                //debugger;
+                if (!result.failure) {
+                    ClosePopupEditor();
+                    
+                    if (UseId) {
+                        if (Id == 'new') {
+                            var newId = result.id;
+                            LoadPageBody(ReloadAction, ReloadController, true, newId);
+                        }
+                        else {
+                            LoadPageBody(ReloadAction, ReloadController, UseId, Id);
+                        }
+
+                    }
+                    else {
+                        LoadPageBody(ReloadAction, ReloadController, false, Id);
+                    }
+                    ShowPopUpSuccessWithTimer("Success!", "Your changes have been saved.");
+                }
+                else {
+                    //swal.close();
+                    ShowPopUpUserError(result.message);
+                }
+                
+                
+            },
+            fail: function (data) {
+                alert("fail");
+            }
+        });
+    }
+
+}
+
+function ShowPopUpSuccessWithTimer(title, htmlMessage) {
+    Swal.fire({
+        //position: 'top-end',
+        type: 'success',
+        title: title,
+        html: "<b>" + htmlMessage + "</b",
+        showConfirmButton: false,
+        timer: 2000
+    });
+}
+
+function LoadPageBody(LoadAction, LoadController, UseId, Id) {
+
+
+
+    var url = getActionURL(LoadAction, LoadController);
+    if (UseId) {
+        url = url + "/" + Id;
+    }
+    
+
+    $("#divGrid").load(url, function () {
+        //this is one sure fire way to always ensure datatables are loaded in partialviews. need to find out why scripts on page aren't working.
+        //$('#detailsTable').dataTable({
+        //    "aaSorting": [],
+        //    "pageLength": 10
+        //});
+    });
+
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+
+
+}
---
+++ CreateDatabaseSchema.ps1
@@ -0,0 +1,208 @@
+﻿function Get-ScriptDirectory {
+    if ($psise) {
+        Split-Path $psise.CurrentFile.FullPath
+    }
+    else {
+        $global:PSScriptRoot
+    }
+}
+
+#Need to fix adding role section
+$sqlScriptPath = Get-ScriptDirectory
+$sqlInstance = "SQL03"
+$databaseName = "AutomationStandards"
+$databasePath = "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\Data" #no \ at end
+$dbOwners = @("Observicing\GS-SQL-Admins")
+
+
+
+#import SqlServer module
+Import-Module -Name "SqlServer"
+
+
+#Check if database exists - currently deleting if so for testing
+$dbCheckSql = "
+DECLARE @dbname nvarchar(128)
+SET @dbname = N'{0}'
+
+IF (EXISTS (SELECT name 
+FROM master.dbo.sysdatabases 
+WHERE ('[' + name + ']' = @dbname 
+OR name = @dbname)))
+
+SELECT 'db exists'" -f $databaseName
+
+
+$dbExists = Invoke-SqlCmd -ServerInstance $sqlInstance -Query $dbCheckSql
+
+if ($dbExists)
+{
+    $dropDatabaseSQL = "USE master; ALTER DATABASE $databaseName SET SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE $databaseName"
+    Invoke-SqlCmd -ServerInstance $sqlInstance -Query $dropDatabaseSQL
+}
+ 
+# create variable with SQL to execute
+$createDbsql = Get-Content "$($sqlScriptPath)\CreateDatabaseSchema.txt"
+$createDbsql = $createDbsql -f $databaseName,$databasePath
+<#
+$createDbsql = "
+CREATE DATABASE {0}
+ CONTAINMENT = NONE
+ ON  PRIMARY
+( NAME = N'{0}', FILENAME = N'{1}\{0}.mdf' , SIZE = 1048576KB , FILEGROWTH = 262144KB )
+ LOG ON
+( NAME = N'{0}_log', FILENAME = N'{1}\{0}_log.ldf' , SIZE = 524288KB , FILEGROWTH = 131072KB )
+GO
+
+USE [master]
+GO
+ALTER DATABASE {0} SET RECOVERY SIMPLE WITH NO_WAIT
+GO
+
+ALTER AUTHORIZATION ON DATABASE::{0} TO [sa]
+
+GO" -f $databaseName,$databasePath
+#>
+#Create Database
+Invoke-SqlCmd -ServerInstance $sqlInstance -Query $createDbsql
+
+
+
+##NEED TO FIX
+<#
+
+
+foreach ($u in $dbOwners)
+{
+    #$authorizationSql = "ALTER AUTHORIZATION ON DATABASE::{0} TO '[{1}]'" -f $databaseName,$u
+    #Invoke-SqlCmd -ServerInstance $sqlInstance -Query $authorizationSql
+    Add-RoleMember -Server $sqlInstance -MemberName "$u" -Database $databaseName -RoleName "db_owner"
+}
+#>
+
+#Create Tables
+$createtablesSql = Get-Content "$($sqlScriptPath)\CreateDatabaseTables.txt"
+$createtablesSql = $createtablesSql -f $databaseName
+Invoke-SqlCmd -ServerInstance $sqlInstance -Query $createtablesSql
+
+
+$sqlDataTypes = Import-Csv "$sqlScriptPath\SQLDataTypes.csv"
+#Populate Tables
+for ($i = 0; $i -lt $sqlDataTypes.Count; $i++)
+{
+    $populateDataTypeSql = "
+    INSERT INTO [{0}].[dbo].[StandardDataType] (DataTypeName,SQLDataType)
+    VALUES ('{1}','{2}')" -f $databaseName,$($sqlDataTypes[$i].DataTypeName),$($sqlDataTypes[$i].SQLDataType)
+    Invoke-SqlCmd -ServerInstance $sqlInstance -Query $populateDataTypeSql
+}
+
+$sqlStdGroups = Import-Csv "$sqlScriptPath\StandardsGroups.csv"
+#Populate Tables
+for ($i = 0; $i -lt $sqlStdGroups.Count; $i++)
+{
+    $populateStdGroupsSql = "
+    INSERT INTO [{0}].[dbo].[StandardGroup] (StandardGroupName)
+    VALUES ('{1}')" -f $databaseName,$($sqlStdGroups[$i].StandardGroupName)
+    Invoke-SqlCmd -ServerInstance $sqlInstance -Query $populateStdGroupsSql
+}
+
+#Create sample data
+<#
+$sqlAdForestLevels = Get-Content "$sqlScriptPath\CreateForestFunctionalLevels.txt"
+$sqlAdForestLevels = $sqlAdForestLevels -f $databaseName
+Invoke-SqlCmd -ServerInstance $sqlInstance -Query $sqlAdForestLevels
+#>
+$sqlSampleTables = Get-Content "$sqlScriptPath\CreateSampleTables.txt"
+$sqlSampleTables = $sqlSampleTables -f $databaseName
+Invoke-SqlCmd -ServerInstance $sqlInstance -Query $sqlSampleTables
+
+
+$sqlAdForestLevelsData = Import-Csv "$sqlScriptPath\ForestFunctionalLevels.csv"
+#Populate Tables
+for ($i = 0; $i -lt $sqlAdForestLevelsData.Count; $i++)
+{
+    $populateSqlAdForestLevelsData = "INSERT INTO [{0}].[dbo].[StForestFunctionalLevels] (CreatedBy, 
+    ModifiedBy, Enabled, SortOrder, ForestFunctionalLevel)
+    VALUES ('{1}','{2}','{3}','{4}','{5}')" -f $databaseName,$sqlAdForestLevelsData[$i].CreatedBy,
+    $sqlAdForestLevelsData[$i].ModifiedBy,$sqlAdForestLevelsData[$i].Enabled,
+    $sqlAdForestLevelsData[$i].SortOrder,$sqlAdForestLevelsData[$i].ForestFunctionalLevel
+   # INSERT INTO [{0}].[dbo].[StForestFunctionalLevels] (CreatedOn, CreatedBy, ModifiedOn, ModifiedBy, Enabled, SortOrder, ForestFunctionalLevel)
+   # VALUES ('{1}','{2}','{3}','{4}','{5}','{6}','{7}')" -f $databaseName,$($sqlAdForestLevelsData[$i].CreatedOn,$sqlAdForestLevelsData[$i].CreatedBy,
+   # $sqlAdForestLevelsData[$i].ModifiedOn,$sqlAdForestLevelsData[$i].ModifiedBy,$sqlAdForestLevelsData[$i].Enabled,$sqlAdForestLevelsData[$i].SortOrder,
+   # $sqlAdForestLevelsData[$i].ForestFunctionalLevel)
+    Invoke-SqlCmd -ServerInstance $sqlInstance -Query $populateSqlAdForestLevelsData
+}
+
+$sqlStdAdDomains = Import-Csv "$sqlScriptPath\ActiveDirectoryDomains.csv"
+#Populate Tables
+for ($i = 0; $i -lt $sqlStdAdDomains.Count; $i++)
+{
+    $populateStdGroupsSql = "
+    INSERT INTO [{0}].[dbo].[StActiveDirectoryDomains] ([DomainShortName],[DomainLongName],[ForestFunctionalLevel],[Enabled],[CreatedBy],[ModifiedBy])
+    VALUES ('{1}','{2}','{3}',{4},'{5}','{6}')" -f $databaseName,$sqlStdAdDomains[$i].DomainShortName,$sqlStdAdDomains[$i].DomainLongName,$sqlStdAdDomains[$i].ForestFunctionalLevel,
+    $sqlStdAdDomains[$i].Enabled,$sqlStdAdDomains[$i].CreatedBy,$sqlStdAdDomains[$i].ModifiedBy
+    Invoke-SqlCmd -ServerInstance $sqlInstance -Query $populateStdGroupsSql
+}
+
+<#
+$sqlForestLevelsStandard = "INSERT INTO [{0}].[DBO].[Standard] (StandardGroupID,DBTableName,	StandardName,	StandardDefinition,	ManageRoles,
+	VersionConfig,	VersionValue,	NotifiyOwner,	ViewerRoles,	UsageCount,	CreatedBy,	ModifiedBy)
+VALUES (2,	'StForestFunctionalLevels',	'Forest Functional Levels',	'AD Forest Functional Levels',	
+'Admin',	0,	0,	0,	'Admin',	0,	'atester', 'atester')" -f $databaseName
+Invoke-SqlCmd -ServerInstance $sqlInstance -Query $sqlForestLevelsStandard
+#>
+$sampleStandardData = Import-Csv "$sqlScriptPath\SampleStandardData.csv"
+for ($i = 0; $i -lt $sampleStandardData.Count; $i++)
+{
+    $sqlStandardData = "INSERT INTO [{0}].[DBO].[Standard] (
+       [StandardGroupID]
+      ,[DBTableName]
+      ,[StandardName]
+      ,[StandardDefinition]
+      ,[ManageRoles]
+      ,[VersionConfig]
+      ,[VersionValue]
+      ,[Tags]
+      ,[NotifiyOwner]
+      ,[OwnerEmail]
+      ,[ViewerRoles]
+      ,[UsageCount]
+      ,[CreatedBy]
+      ,[ModifiedBy]
+      )
+      VALUES ({1},'{2}','{3}','{4}','{5}',{6},{7},{8},{9},{10},'{11}',{12},'{13}','{14}')" -f $databaseName,$sampleStandardData[$i].StandardGroupID,$sampleStandardData[$i].DBTableName,
+      $sampleStandardData[$i].StandardName,$sampleStandardData[$i].StandardDefinition,$sampleStandardData[$i].ManageRoles,$sampleStandardData[$i].VersionConfig,
+      $sampleStandardData[$i].VersionValue,$sampleStandardData[$i].Tags,$sampleStandardData[$i].NotifiyOwner,$sampleStandardData[$i].OwnerEmail,
+      $sampleStandardData[$i].ViewerRoles,$sampleStandardData[$i].UsageCount,$sampleStandardData[$i].CreatedBy,$sampleStandardData[$i].ModifiedBy
+      Invoke-SqlCmd -ServerInstance $sqlInstance -Query $sqlStandardData
+}
+
+#Run This Last
+$sampleStandardConfigs = Import-Csv "$sqlScriptPath\SampleStandardConfigs.csv"
+for ($i = 0; $i -lt $sampleStandardConfigs.Count; $i++)
+{
+    $sqlStandardConfig = "INSERT INTO [{0}].[DBO].[StandardConfig] (      
+           [StandardID]
+          ,[FieldName]
+          ,[DisplayName]
+          ,[DataTypeID]
+          ,[SortOrder]
+          ,[VersionNumber]
+          ,[UseToolTip]
+          ,[ToolTip]
+          ,[UseStandardData]
+          ,[AllowMultiSelect]
+          ,[StandardLUID]
+          ,[StandardLUValue]
+          ,[StandardUseFilter]
+          ,[StandardFilterSQL]
+          ,[CreatedBy]
+          ,[ModifiedBy]
+          )
+          VALUES ({1},'{2}','{3}',{4},{5},{6},{7},'{8}',{9},{10},{11},'{12}',{13},'{14}','{15}','{16}')" -f $databaseName,$sampleStandardConfigs[$i].StandardId,$sampleStandardConfigs[$i].FieldName,
+          $sampleStandardConfigs[$i].DisplayName,$sampleStandardConfigs[$i].DataTypeID,$sampleStandardConfigs[$i].SortOrder,$sampleStandardConfigs[$i].VersionNumber,
+          $sampleStandardConfigs[$i].UseToolTip,$sampleStandardConfigs[$i].ToolTip,$sampleStandardConfigs[$i].UseStandardData,$sampleStandardConfigs[$i].AllowMultiSelect,
+          $sampleStandardConfigs[$i].StandardLUID,$sampleStandardConfigs[$i].StandardLUValue,$sampleStandardConfigs[$i].StandardUseFilter,
+          $sampleStandardConfigs[$i].StandardFilterSQL,$sampleStandardConfigs[$i].CreatedBy,$sampleStandardConfigs[$i].ModifiedBy
+          Invoke-SqlCmd -ServerInstance $sqlInstance -Query $sqlStandardConfig
+}
